da462400d372a7edbaf7a5fcc2a5570649120785
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-11 11:05:52 +0300

    Adding Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv
 
 if __name__ == "__main__":
     size = len(argv)
-    if size == 0:
+    if size == 1:
         print("0 arguments.")
     elif size == 2:
         print("{} argument:".format(size - 1))
