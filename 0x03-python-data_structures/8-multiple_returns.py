d8fd4daa6e4c1ca47cddb7b566d1cf451427d2b6
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-13 23:56:57 +0300

    Adding Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    if len(sentence) != 0 and sentence[0]:
+        return len(sentence), sentence[0]
+    else:
+        return 0, None
+
+
+sentence = "aewighsopih"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
