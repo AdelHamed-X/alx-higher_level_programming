6acae15f957c31a4055358364786ad3f366c03c9
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-09-13 15:25:48 +0300

    Adding Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for c in len(my_string):
+    for c in range(len(my_string)):
         if my_string[c] in ('c', 'C'):
             continue
         my_string[c] = my_string[c]
