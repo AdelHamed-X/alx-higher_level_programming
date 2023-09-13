5686ee619668048bc94a0c65a6bc757f9175e923
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-14 00:14:32 +0300

    Adding Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    check = my_list[0]
+    if len(my_list) == 0:
+        return None
+    for integer in my_list:
+        if integer > check:
+            check = integer
+    return check
