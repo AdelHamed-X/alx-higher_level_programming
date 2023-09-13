a48c485fb7a0629b55ce3f982cc05f58ef933b82
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-09-13 11:19:28 +0300

    Adding Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,23 +4,21 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-    if len(argv) != 4:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    def calc(a, operator, b):
-        a, operator, b, result = int(argv[1]), argv[2], int(argv[3]), 0
-        if operator not in ['+', '-', '*', '/']:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        if operator == "+":
-            result = add(a, b)
-        elif operator == "-":
-            result = sub(a, b)
-        elif operator == "*":
-            result = mul(a, b)
-        elif operator == "/":
-            result = div(a, b)
-        print("{} {} {} = {}".format(a, operator, b, result))
-        exit(0)
-    calc(argv[1], argv[2], argv[3])
+    a, operator, b, result = int(argv[1]), argv[2], int(argv[3]), 0
+    if operator not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
+    exit(0)
