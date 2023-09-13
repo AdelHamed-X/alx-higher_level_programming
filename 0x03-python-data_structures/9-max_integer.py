e66a9984e74fa867b97e25ace09771d2913f96b7
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-14 00:19:09 +0300

    Adding Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,8 +8,3 @@ def max_integer(my_list=[]):
             if integer > check:
                 check = integer
         return check
-
-
-my_list = []
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
