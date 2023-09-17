6d6ea8979d8f57d87e28e94833c9a4eff85750c4
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-17 15:57:37 +0300

    Adding Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    my_list = [my_list[i] for i in range(len(my_list)) if i != idx]
+    else:
+        my_list = [my_list[i] for i in range(len(my_list)) if i != idx]
     return my_list
