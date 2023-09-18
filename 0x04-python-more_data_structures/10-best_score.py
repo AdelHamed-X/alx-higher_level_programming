c13508ed075366fc855d401e158939540563d34b
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-18 23:13:56 +0300

    Adding Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     check = 0
     max = ""
+    if len(list(a_dictionary)) == 0:
+        return None
     for key in list(a_dictionary):
         if a_dictionary[key] > check:
             check = a_dictionary[key]
