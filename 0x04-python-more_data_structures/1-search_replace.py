2c5c97bb00e55c2de6dda9e1a41f7c69af7d7b04
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-18 21:50:18 +0300

    Adding Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,6 @@ def search_replace(my_list, search, replace):
     for elem in my_list:
         if elem == search:
             newlist.append(replace)
+            continue
         newlist.append(elem)
     return newlist
