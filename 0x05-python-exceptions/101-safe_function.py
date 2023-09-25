4218c3ce35710bc18dc88795ec49e076deb75091
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-26 01:43:32 +0300

    Adding Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except (RuntimeError, TypeError, ZeroDivisionError, IndexError) as err:
+    except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return None
