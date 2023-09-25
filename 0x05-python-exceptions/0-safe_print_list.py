8bfa557fbb9ef09464377ae966481d5c8b97a26e
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-25 23:48:44 +0300

    Adding Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,6 +11,4 @@ def safe_print_list(my_list=[], x=0):
         print()
     except NameError as src:
         print("NameError: {}".format(src))
-
-    finally:
-        return elem_number
+    return elem_number
