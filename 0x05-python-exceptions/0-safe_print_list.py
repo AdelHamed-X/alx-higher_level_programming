559a2bfd1d346eff663c32d871b7e77841532e71
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-25 23:53:21 +0300

    Adding Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     elem_number = 0
     for idx in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[idx]), end="")
             elem_number += 1
         except IndexError:
             break
