1a1dc861b46328b4a354e7b9c8c3c121a6c3d641
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-27 01:16:50 +0300

    Adding Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Initiating an instance with
+    size attribute"""
+    def __init__(self, size):
+        self.size = size
+        pass
