ef3002d6acbe9de9f08f8315e51dfcf3d43ebf36
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-29 15:13:28 +0300

    Adding Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
     """ def position(self, value): setter for priv. attr position (Setter) """
     @position.setter
     def position(self, value):
-        if isinstance(value, tuple):
+        if isinstance(value, tuple) and len(value) == 2:
             if value[0] >= 0 and value[1] >= 0:
                 self.__position = value
             else:
