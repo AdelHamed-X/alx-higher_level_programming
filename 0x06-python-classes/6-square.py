0a58c47989f23ad9fa0fd9c4b56ac60da8747de9
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-29 15:00:40 +0300

    Adding Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,9 +50,13 @@ class Square:
         while self.size > i:
             j = 0
             k = 1
+            m = 0
             while self.position[0] > k:
                 print("_", end="")
                 k += 1
+            while self.position[1] > m:
+                print()
+                m += 1
             while self.size > j:
                 print("#", end="")
                 j += 1
