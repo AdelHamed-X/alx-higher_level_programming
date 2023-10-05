7cf6f6f56c9d2152eb4a690c058fbb5d0f95f828
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-05 17:50:39 +0300

    Adding Task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Unittests for max_integer([..])."""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer([..])."""
+
+    def test_ordered_list(self):
+        """Test an ordered list of integers."""
+        ordered = [1, 2, 3, 4]
+        self.assertEquals(max_integer(ordered), 4)
+
+    def test_unordered_list(self):
+        """Test an unordered list of integers."""
+        unordered = [7, 4, 3, 2]
+        self.assertEquals(max_integer(unordered), 7)
