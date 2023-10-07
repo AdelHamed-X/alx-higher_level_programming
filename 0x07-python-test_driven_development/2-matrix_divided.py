b554ecfcfbfac88c12e84639eecb647e5b1f736b
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-07 17:54:40 +0300

    Task 1 Fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,8 @@ by a number (div)"""
 
 def matrix_divided(matrix, div):
     """
-    All elements of the matrix should be divided by div, rounded to 2 decimal places
+    All elements of the matrix should be divided by div,
+    rounded to 2 decimal places
     :param matrix: a list of lists of integers or floats
     :param div: a number (integer or float)
     :return: a new matrix
@@ -14,7 +15,8 @@ def matrix_divided(matrix, div):
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(element, float) or isinstance(element, int))
                     for element in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
 
     for row in matrix:
         if len(matrix[0]) != len(row):
