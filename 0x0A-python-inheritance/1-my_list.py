c25d7fac61ba77fbf92b908120f4f5be85b2d176
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-09 22:55:03 +0300

    Adding Tasks 2 - 9

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,12 +8,23 @@ class MyList(list):
     """
     A subclass of 'list' parent class
     """
+    def __init__(self):
+        super().__init__()
+
     def print_sorted(self):
         """
         prints a sorted list
         :return: sort list
         """
-        newlist = []
-        for element in self:
-            newlist.append(element)
-        return print(newlist.sort())
+        return print(sorted(self))
+
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
