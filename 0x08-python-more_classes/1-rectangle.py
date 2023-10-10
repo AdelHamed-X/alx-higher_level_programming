ac49c6d195b05d3c36dee46743870690a73002cd
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-10-10 11:32:00 +0300

    Adding Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module creates a rectangle class
+"""
+
+
+class Rectangle:
+    """class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width Getter"""
+        return self.__width__
+
+    @width.setter
+    def width(self, value):
+        """width Setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width__ = value
+
+    @property
+    def height(self):
+        """height Getter"""
+        return self.__height__
+
+    @height.setter
+    def height(self, value):
+        """height Setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height__ = value
