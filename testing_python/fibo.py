75564b519fec1d58b233cd288d3c9d27e140f815
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-14 17:54:52 +0300

    Adding Tasks 29

--- testing_python/fibo.py
+++ testing_python/fibo.py
@@ -1,20 +1,7 @@
-# Fibonacci numbers module
+#!/usr/bin/python3
 
-def fib(n):    # write Fibonacci series up to n
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
-    print()
+x = 1
+y = 2
 
-def fib2(n):   # return Fibonacci series up to n
-    result = []
-    a, b = 0, 1
-    while a < n:
-        result.append(a)
-        a, b = b, a+bv
-    return result
+print(x is y)
 
-if __name__ == "__main__":
-    import sys
-    fib(int(sys.argv[1]))
