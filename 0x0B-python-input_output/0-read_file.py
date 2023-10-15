805a9badbb0ba735f78b4b4d0d144a7c6fafd849
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-15 20:38:47 +0300

    Adding Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,6 @@ def read_file(filename=""):
     :return: None
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
+        print('\n')
