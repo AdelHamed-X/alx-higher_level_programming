23d17b8a80381cf3a8425d0a26871d23fb82cd2a
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-15 22:29:09 +0300

    Adding Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module has a function that creates an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”
+    :param filename: the destination file
+    :return: None
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            return json.loads(line)
