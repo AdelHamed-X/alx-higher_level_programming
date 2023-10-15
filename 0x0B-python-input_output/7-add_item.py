3dbbf3cb88fc1951c25837eebd6d61037dab8675
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-15 23:01:25 +0300

    Adding Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-This module has a python script that adds all
-arguments to a Python list, and then save them to a file
-"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
