28cce26a7564de8e3139886676d80e36ccd66822
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-16 09:35:43 +0300

    Adding Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module has a function that returns __dict__
+of a class object
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    :param obj: class object
+    """
+    return obj.__dict__
