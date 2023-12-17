2e11b739226099e087e1a84209bea5f911062d14
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-17 23:27:36 +0200

    Adding Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,11 +13,9 @@ db = MySQLdb.connect(host='localhost',
                      port=3306)
 
 if __name__ == '__main__':
-    db_cursor = db.cursor()
-
-    db_cursor.execute("SELECT * FROM states")
-
-    rows = db_cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
