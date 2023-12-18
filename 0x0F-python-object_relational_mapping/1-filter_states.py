4280ff72c217c1e08b377446d93a6695f6a2d234
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-18 22:49:37 +0200

    Adding Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,15 +6,17 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                            passwd=sys.argv[2],
+                            db=sys.argv[3], port=3306)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' COLLATE utf8mb4_bin;")
-    states = cur.fetchall()
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%'
+            COLLATE utf8mb4_bin;")
+    states = c.fetchall()
 
     for state in states:
         print(state)
 
-    cur.close()
+    c.close()
     db.close()
