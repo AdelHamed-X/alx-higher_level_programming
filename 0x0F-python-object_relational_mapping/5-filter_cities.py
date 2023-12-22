1bd65188238ea7037c2ee0899673af92b846abf4
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-22 13:05:37 +0200

    Adding Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+
+    state = argv[4]
+
+    cur = db.cursor()
+    cur.execute(";")
