ec4a6e34aeac9333f32ff83518c53376670b1aaa
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-22 10:15:42 +0200

    Adding Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,12 +2,12 @@
 """
 Lists all cities from the database hbtn_0e_0_usa
 """
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
