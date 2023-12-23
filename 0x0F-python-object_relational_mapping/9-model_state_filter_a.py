e63d687cd231f9d313171ddecdbd1c9121bc2a06
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-24 00:46:49 +0200

    Adding Task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa """
+
+from sqlalchemy import create_engine, text
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+
+    usr, pwd, db = argv[1:]
+
+    engine = create_engine(f"mysql+mysqldb://{usr}:{pwd}@localhost/{db}")
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT states.id, states.name FROM states"
+                              " WHERE name LIKE '%a%' ORDER BY states.id;"))
+    
+    for state in result:
+        print(f"{state.id}: {state.name}")
