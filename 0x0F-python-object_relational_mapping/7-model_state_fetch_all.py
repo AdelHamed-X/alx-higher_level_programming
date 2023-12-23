823d4095c5be7e2f3494e0c482a2a72fed3a999a
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-23 22:10:38 +0200

    Adding Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 """ A script that lists all State objects from the database hbtn_0e_6_usa """
 
+from sqlalchemy import create_engine, text()
 
+if __name__ == '__main__':
+    from model_state import Base, State
+
+    engine = create_engine()
+    with engine.connection() as conn:
+        result = conn.execute(text("SELECT states.id, states.name FROM states\
+                                   ORDER BY states.id;"))
+
+    for state in result:
+        print(f"{state[0]}: {state[1]}")
