77aa0e02868b5e14c2a0c58718c473e29519daef
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-26 12:25:41 +0200

    Adding Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     session = Session(bind=engine)
 
     result = session.execute(text("SELECT states.id FROM states "
-                             "WHERE states.name LIKE :state;"),\
+                             "WHERE states.name LIKE :state;"),
                                 {"state": state_name})
 
     state = result.fetchone()
