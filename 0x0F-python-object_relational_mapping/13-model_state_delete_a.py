074a01b65a293987105d985c7080c44a9f90a890
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-29 14:34:37 +0200

    Adding Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,13 +7,14 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-usr, pwd, db = argv[1:]
+if __name__ == '__main__':
+    usr, pwd, db = argv[1:]
 
-engine = create_engine(f"mysql+mysqldb://{usr}:{pwd}@localhost/{db}")
-Session = sessionmaker()
-session = Session(bind=engine)
+    engine = create_engine(f"mysql+mysqldb://{usr}:{pwd}@localhost/{db}")
+    Session = sessionmaker()
+    session = Session(bind=engine)
 
-stmt = delete(State).where(State.name.like('%a%'))
+    stmt = delete(State).where(State.name.like('%a%'))
 
-with session.begin():
-    session.execute(stmt)
+    with session.begin():
+        session.execute(stmt)
