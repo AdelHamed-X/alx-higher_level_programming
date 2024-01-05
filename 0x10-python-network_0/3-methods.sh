a875cf6ae7f31bbb8634bf3debb953e66f334027
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-05 15:06:46 -0800

    Adding Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that shows all allowed methods to the server.
-curl -s -i -X OPTIONS "$1" | grep Allow | cut -d ' ' -f 2
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d ' ' -f 2
