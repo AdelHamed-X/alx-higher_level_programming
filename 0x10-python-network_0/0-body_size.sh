27a5537ad8b0622a9a80a3e307487a84cfbb960d
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-05 08:25:39 -0800

    Adding Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # A script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
-curl "$1" | grep "Content-Length" | cut " " -f 2
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
