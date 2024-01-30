ad9a7b50fcb60573ec7af5b2f2d253d9f58f11b1
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-30 11:38:55 +0200

    Adding Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,8 +9,9 @@ from urllib import request, parse
 from sys import argv
 
 
-data = parse.urlencode({'email': f'{argv[2]}'})
-data = data.encode('ascii')
+if __name__ == "__main__":
+    data = parse.urlencode({'email': f'{argv[2]}'})
+    data = data.encode('ascii')
 
-with request.urlopen(f'{argv[1]}', data)as resp:
-    print(data.read().decode('utf-8'))
+    with request.urlopen(f'{argv[1]}', data)as resp:
+        print(data.read().decode('utf-8'))
