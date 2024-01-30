ca38b5c74af4e67c746b59cefbb79eb23ebc4b47
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-30 14:20:50 +0200

    Adding Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    request = post(argv[1], data={"email": {argv[2]}})
+    request = post(argv[1], {"email": {argv[2]}})
     print(request.text)
