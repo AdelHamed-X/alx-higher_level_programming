48eae947711c58f19cd81d9d1e59d34178dcb993
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-30 13:35:28 +0200

    Adding Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,8 +2,9 @@
 """ A Python script that fetches https://alx-intranet.hbtn.io/status """
 
 import requests
+from sys import argv
 
 
 if __name__ == '__main__':
-    resp = requests.get("https://alx-intranet.hbtn.io/status")
+    resp = requests.get(argv[1])
     print(resp.headers["X-Request-Id"])
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A Python script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response """
+
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = parse.urlencode({'email': f'{argv[2]}'})
+    data = data.encode('ascii')
+
+    with request.urlopen(f'{argv[1]}', data)as resp:
+        print(resp.read().decode('utf-8'))
