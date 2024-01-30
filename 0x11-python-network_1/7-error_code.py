c65f7126bb8e92cfcbd2aec6745b715eaced3f22
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-30 14:27:59 +0200

    Adding Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A Python script that takes in a URL,
+sends a request to the URL and displays the body of the response. """
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    resp = requests.get(argv[1])
+    if resp.status_code >= 400:
+        print(f"Error code: {resp.status_code}")
+    else:
+        print(resp.text)
