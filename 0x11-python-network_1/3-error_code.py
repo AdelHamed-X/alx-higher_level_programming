3cd35d35f153e80c2b97be43727d1e332c8d134d
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-30 12:16:54 +0200

    Adding Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,14 +2,16 @@
 """ Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8). """
 
-from urllib import request
-from urllib.error import HTTPError
-from sys import argv
 
 
 if __name__ == '__main__':
+    from urllib import request
+    from urllib.error import HTTPError
+    from sys import argv
+
+    
     try:
-        with request.urlopen(f'{argv[1]}') as response:
+        with request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as e:
         print(f'Error code: {e.code}')
