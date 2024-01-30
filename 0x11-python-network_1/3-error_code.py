020be5e5481a8609d456ce8fda2193a4c8593ac1
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-30 12:06:50 +0200

    Adding Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,10 +6,10 @@ from urllib import request
 from urllib.error import HTTPError
 from sys import argv
 
+
 if __name__ == '__main__':
     with request.urlopen(f'{argv[1]}') as response:
         try:
-            print(response.read())
+            print(response.read().decode('utf-8'))
         except HTTPError as e:
             print(f'Error code: {e.code}')
-            
