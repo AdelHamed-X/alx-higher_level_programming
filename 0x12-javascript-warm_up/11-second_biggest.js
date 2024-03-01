60a5882b68e9511d62c25bd94099edb33b27fae9
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-03-01 04:51:27 +0200

    Adding Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2).map(Number);
 if (args.length < 2) {
   console.log(0);
 } else {
-   sorted = args.sort(function (a, b) {
+  const sorted = args.sort(function (a, b) {
     return a - b;
   });
   console.log(sorted[args.length - 2]);
