9c877d812f6921386ca9ccafc6dc31ebab09e7fa
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-03-01 22:04:19 +0200

    Adding Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,5 @@
 const fs = require('fs');
 
 fs.readFile(`${process.argv[2]}`, 'utf-8', (err, data) => {
-  if (err) {console.log(err)}
-  else {console.log(data)};
-})
+  if (err) { console.log(err); } else { console.log(data); }
+});
