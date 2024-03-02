1467a4552fc71bf2d1e38730fa33360b170938ab
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-03-02 02:38:23 +0200

    Adding Task 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 $('#toggle_header').click(function () {
-  if ($('header').hasClass('red') && $('header').hasClass('green')) {
-    $('header').removeClass('red');
-  } else if (!$('header').hasClass('red') && !$('header').hasClass('green')) {
-    $('header').addClass('red');
-  } else if ($('header').hasClass('red')) {
-    $('header').removeClass('red');
-    $('header').addClass('green');
-  } else if ($('header').hasClass('green')) {
-    $('header').removeClass('green');
-    $('header').addClass('red');
+  const header = header;
+
+  if (header.hasClass('red') && header.hasClass('green')) {
+    header.removeClass('red');
+  } else if (!header.hasClass('red') && !header.hasClass('green')) {
+    header.addClass('red');
+  } else if (header.hasClass('red')) {
+    header.removeClass('red');
+    header.addClass('green');
+  } else if (header.hasClass('green')) {
+    header.removeClass('green');
+    header.addClass('red');
   }
 });
